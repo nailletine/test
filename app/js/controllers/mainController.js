7ab331c16f32f00f70e80a1d144093e88469741b
nailletine <nailletine.lajoie19@gmail.com>
2017-07-06 16:59:29 +0200

    architecture done +appel api ok

--- app/js/controllers/mainController.js
+++ app/js/controllers/mainController.js
@@ -1,17 +1,22 @@
 angular.module('app')
     .controller('MainController', function($scope, ApiService) {
-  ApiService.getMovie();
+        $scope.currentCollection = {};
+        $scope.loadCollection = function(id) {
+            ApiService.getCollection(id).then(function(res) {
+                $scope.currentCollection = res.data;
+            });
+        };
+        $scope.displayMovie = function (movie){
+          $scope.currentMovie = movie;
+        }; 
 
-  $scope.collections=[
-    {
-      id:10,
-      name: "Star Wars collection"
-    },
-    {
-        id: 8945,
-        name: "Mad max collection"
-    }
-
-
-  ];
+        $scope.collections = [{
+                id: 10,
+                name: "Star Wars collection"
+            },
+            {
+                id: 8945,
+                name: "Mad max collection"
+            }
+        ];
     });
